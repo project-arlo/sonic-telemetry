bc002fbd32a4a41a4f370bd760bd2b2bdade8859
Eric Seifert <seiferteric@gmail.com>
2020-02-10 12:12:15 -0800

    Always call PopulateAuthStruct to set roles

--- src/gnmi_server/basicAuth.go
+++ src/gnmi_server/basicAuth.go
@@ -1,12 +1,12 @@
 package gnmi_server
 
 import (
-	"golang.org/x/net/context"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"github.com/golang/glog"
 	"common_utils"
+	"github.com/golang/glog"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 func BasicAuthenAndAuthor(ctx context.Context) (context.Context, error) {
@@ -15,28 +15,28 @@ func BasicAuthenAndAuthor(ctx context.Context) (context.Context, error) {
 	if !ok {
 		return ctx, status.Errorf(codes.Unknown, "Invalid context")
 	}
-	
+
 	var username string
 	var passwd string
 	if username_a, ok := md["username"]; ok {
 		username = username_a[0]
-	}else {
+	} else {
 		return ctx, status.Errorf(codes.Unauthenticated, "No Username Provided")
 	}
 
 	if passwd_a, ok := md["password"]; ok {
 		passwd = passwd_a[0]
-	}else {
+	} else {
 		return ctx, status.Errorf(codes.Unauthenticated, "No Password Provided")
 	}
-	if err := PopulateAuthStruct(username, &rc.Auth); err != nil {
+	if err := PopulateAuthStruct(username, &rc.Auth, nil); err != nil {
 		glog.Infof("[%s] Failed to retrieve authentication information; %v", rc.ID, err)
-		return ctx, status.Errorf(codes.Unauthenticated, "")	
+		return ctx, status.Errorf(codes.Unauthenticated, "")
 	}
 	auth_success, _ := UserPwAuth(username, passwd)
 	if auth_success == false {
-		return ctx, status.Errorf(codes.PermissionDenied, "Invalid Password")	
+		return ctx, status.Errorf(codes.PermissionDenied, "Invalid Password")
 	}
-	
+
 	return ctx, nil
 }
